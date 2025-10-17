52f5069be08506e145a827e53ea2f0c0bfea48e1
Davi Borges <davi.borges@gs3tecnologia.com.br>
2025-10-17 17:10:05 -0300

    rules

--- lib/presentation/rules/rules_view.dart
+++ lib/presentation/rules/rules_view.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../resource/assets_manager.dart';
+import '../resource/color_manager.dart';
+import '../resource/screen_manager.dart';
+
 class RulesView extends StatelessWidget {
   const RulesView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorManager.black,
       appBar: AppBar(
-        title: const Text('Rules'),
+        iconTheme: IconThemeData(color: ColorManager.white),
+        backgroundColor: ColorManager.black,
+        titleSpacing: 0,
+        leadingWidth: 40,
+        title: SizedBox(
+          width: context.percentWidth(.3),
+          child: Image.asset(ImageAssets.logoSplash),
+        ),
       ),
       body: Container(),
     );
