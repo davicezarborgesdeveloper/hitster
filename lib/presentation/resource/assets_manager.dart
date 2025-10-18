57ce823f5c9988dfe35bd9f3561209de9843f23e
Davi Borges <davi.borges@gs3tecnologia.com.br>
2025-10-18 08:57:17 -0300

    pontual

--- lib/presentation/resource/assets_manager.dart
+++ lib/presentation/resource/assets_manager.dart
@@ -3,6 +3,7 @@ const String jsonPath = 'assets/json';
 
 class ImageAssets {
   static const String logoSplash = '$imagePath/splash_logo.png';
+  static const String rulesTopImage = '$imagePath/rules-image.png';
 }
 
 class JsonAssets {
