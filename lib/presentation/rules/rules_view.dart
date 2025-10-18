5dca660cbb8cfe08f4bc94a8f014b0df8e9f804f
Davi Borges <davi.borges@gs3tecnologia.com.br>
2025-10-18 11:06:41 -0300

    imagem regra

--- lib/presentation/rules/rules_view.dart
+++ lib/presentation/rules/rules_view.dart
@@ -4,9 +4,14 @@ import '../resource/assets_manager.dart';
 import '../resource/color_manager.dart';
 import '../resource/screen_manager.dart';
 
-class RulesView extends StatelessWidget {
+class RulesView extends StatefulWidget {
   const RulesView({super.key});
 
+  @override
+  State<RulesView> createState() => _RulesViewState();
+}
+
+class _RulesViewState extends State<RulesView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +26,19 @@ class RulesView extends StatelessWidget {
           child: Image.asset(ImageAssets.logoSplash),
         ),
       ),
-      body: Container(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: false,
+            expandedHeight: 160.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text("FlexibleSpace title"),
+              background: Image.asset(ImageAssets.rulesTopImage),
+            ),
+          ),
+          const SliverFillRemaining(child: Center(child: Text("Hello"))),
+        ],
+      ),
     );
   }
 }
